bcc707da1cf219ba0272e9b28edeec5981345b45
mahdieh-amiri1 <mahdieh.amiri.it@gmail.com>
2024-10-31 18:55:53 +0330

    Bulk update files

--- contracts/MyOApp.sol
+++ contracts/MyOApp.sol
@@ -1,70 +1,52 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
 
-pragma solidity ^0.8.22;
-
+import { OApp, Origin, MessagingFee } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { OApp, MessagingFee, Origin } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
-import { MessagingReceipt } from "@layerzerolabs/oapp-evm/contracts/oapp/OAppSender.sol";
 
 contract MyOApp is OApp {
-    constructor(address _endpoint, address _delegate) OApp(_endpoint, _delegate) Ownable(_delegate) {}
+    constructor(address _endpoint, address _owner) OApp(_endpoint, _owner) Ownable(_owner) {}
 
-    string public data = "Nothing received yet.";
-
-    /**
-     * @notice Sends a message from the source chain to a destination chain.
-     * @param _dstEid The endpoint ID of the destination chain.
-     * @param _message The message string to be sent.
-     * @param _options Additional options for message execution.
-     * @dev Encodes the message as bytes and sends it using the `_lzSend` internal function.
-     * @return receipt A `MessagingReceipt` struct containing details of the message sent.
-     */
-    function send(
-        uint32 _dstEid,
-        string memory _message,
-        bytes calldata _options
-    ) external payable returns (MessagingReceipt memory receipt) {
-        bytes memory _payload = abi.encode(_message);
-        receipt = _lzSend(_dstEid, _payload, _options, MessagingFee(msg.value, 0), payable(msg.sender));
-    }
+    // Some arbitrary data you want to deliver to the destination chain!
+    string public data;
 
     /**
-     * @notice Quotes the gas needed to pay for the full omnichain transaction in native gas or ZRO token.
+     * @notice Sends a message from the source to destination chain.
      * @param _dstEid Destination chain's endpoint ID.
-     * @param _message The message.
+     * @param _message The message to send.
      * @param _options Message execution options (e.g., for sending gas to destination).
-     * @param _payInLzToken Whether to return fee in ZRO token.
-     * @return fee A `MessagingFee` struct containing the calculated gas fee in either the native token or ZRO token.
      */
-    function quote(
-        uint32 _dstEid,
-        string memory _message,
-        bytes memory _options,
-        bool _payInLzToken
-    ) public view returns (MessagingFee memory fee) {
-        bytes memory payload = abi.encode(_message);
-        fee = _quote(_dstEid, payload, _options, _payInLzToken);
+    function send(uint32 _dstEid, string memory _message, bytes calldata _options) external payable {
+        // Encodes the message before invoking _lzSend.
+        // Replace with whatever data you want to send!
+        bytes memory _payload = abi.encode(_message);
+        _lzSend(
+            _dstEid,
+            _payload,
+            _options,
+            // Fee in native gas and ZRO token.
+            MessagingFee(msg.value, 0),
+            // Refund address in case of failed source message.
+            payable(msg.sender)
+        );
     }
 
     /**
-     * @dev Internal function override to handle incoming messages from another chain.
-     * @dev _origin A struct containing information about the message sender.
-     * @dev _guid A unique global packet identifier for the message.
-     * @param payload The encoded message payload being received.
-     *
-     * @dev The following params are unused in the current implementation of the OApp.
-     * @dev _executor The address of the Executor responsible for processing the message.
-     * @dev _extraData Arbitrary data appended by the Executor to the message.
-     *
-     * Decodes the received payload and processes it as per the business logic defined in the function.
+     * @dev Called when data is received from the protocol. It overrides the equivalent function in the parent contract.
+     * Protocol messages are defined as packets, comprised of the following parameters.
+     * @param _origin A struct containing information about where the packet came from.
+     * @param _guid A global unique identifier for tracking the packet.
+     * @param payload Encoded message.
      */
     function _lzReceive(
-        Origin calldata /*_origin*/,
-        bytes32 /*_guid*/,
+        Origin calldata _origin,
+        bytes32 _guid,
         bytes calldata payload,
-        address /*_executor*/,
-        bytes calldata /*_extraData*/
+        address, // Executor address as specified by the OApp.
+        bytes calldata // Any extra data or options to trigger on receipt.
     ) internal override {
+        // Decode the payload to get the message
+        // In this case, type is string, but depends on your encoding!
         data = abi.decode(payload, (string));
     }
 }
